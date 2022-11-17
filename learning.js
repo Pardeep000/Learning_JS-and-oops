f058600d431be7873ca81b039e29f1e155b61865
Pardeep Kumar <pardeepkumar1727@gmail.com>
2022-11-18 00:08:40 +0500

    JS classes with their instances viz objects

--- learning.js
+++ learning.js
@@ -1,4 +1,12 @@
 class Car {
+
+    static announcement() {
+        console.log('new model');
+        this.name = "Bugatti";
+        // this.model = '2022';
+    }
+
+
     constructor(name, year) {
         this.name = name;
         this.model = year;
@@ -6,22 +14,36 @@ class Car {
     #defaultSpeed = '5mph';
     #boolmove = false;
     #movingSpeed = this.#defaultSpeed;
+    //
+    get _defaultSpeed() {
+        return this.#defaultSpeed;
+    }
+    set _defaultSpeed(value) {
+        this.#defaultSpeed = value;
+    }
+    //
     move(speed) {
         if (this.#boolmove == false) console.log('car is not moving, first you have to start it...');
         else console.log('car is moving at ', this.#movingSpeed);
     }
-    changeSpeed(value){
+    changeSpeed(value) {
         this.#movingSpeed = value;
         console.log('car speed is changed to ', this.#movingSpeed);
     }
     stop() {
-        this.#boolmove=false;
-        this.#movingSpeed='0mph';
+        this.#boolmove = false;
+        this.#movingSpeed = '0mph';
         console.log('car is stopped');
     }
     start() {
-        console.log(`car has been started and moving with default Speed of `, this.#defaultSpeed);
-        this.#boolmove = true;
+        if (this.#boolmove == false) {
+            console.log(`car has been started and moving with default Speed of `, this.#defaultSpeed);
+            this.#boolmove = true;
+            this.#movingSpeed = this.#defaultSpeed;
+        }
+        else {
+            console.log(`car is already moving at `, this.#defaultSpeed);
+        }
     }
 
     changeDefaultSpeed(value) {
@@ -31,17 +53,16 @@ class Car {
 }
 
 let car1 = new Car('lamborghini', 2020);
-console.log(car1);
-car1.move()
+car1.start()
+car1.stop()
+car1._defaultSpeed = '18mph'
 car1.start()
 car1.move()
-car1.changeSpeed('10mph')
-car1.move()
+car1.start()
 car1.stop()
 car1.move()
 car1.start()
-car1.stop()
-car1.move()
-car1.changeDefaultSpeed('50mph')
-car1.move()
-car1.start()
+
+
+Car.announcement();
+// console.log(car1.name);
