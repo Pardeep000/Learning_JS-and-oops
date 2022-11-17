1059a4a75b2a34262b92b153c0825451d55134ea
= <=>
2022-11-17 18:34:55 +0500

    complete three approaches for object declaration

--- learning.js
+++ learning.js
@@ -1,20 +1,32 @@
-//By factory function
-function createCircle(radius, x, y) {
-    return {
-        radius: radius,
-        location: {
-            x: x,//just x
-            y: y,// just y
-        },
-        draw: function () {
-            console.log(`circle is drawn with features\nradius= ${radius} and point coordinates=${x, y}`)
-        }
+//By constructor function
+function Circle(rad){
+    this.radius = rad;
+    this.draw = function(){
+        console.log(`circle drawn with radius => ${rad}`);
     }
 }
 
+let circle = new Circle(5);
+console.log(circle.draw());
 
-let circle = createCircle(2, 1, 1);
-circle.draw();
+
+// //By factory function
+// function createCircle(radius, x, y) {
+//     return {
+//         radius: radius,
+//         location: {
+//             x: x,//just x
+//             y: y,// just y
+//         },
+//         draw: function () {
+//             console.log(`circle is drawn with features\nradius= ${radius} and point coordinates=${x, y}`)
+//         }
+//     }
+// }
+
+
+// let circle = createCircle(2, 1, 1);
+// circle.draw();
 
 
 // //By literal notation
